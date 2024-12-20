6fb81c10e63c0347250ea89bfc8008d0eb67f7eb
Sr-Mourao <felipemourao6124@gmail.com>
2024-12-19 23:31:39 -0300

    console style

---
+++ scripts/console-style.js
@@ -0,0 +1,5 @@
+const x = "ATENÇÃO!!!";
+const d =
+  "Muito cuidado ao abrir o console, esta é uma ferramenta para desenvolvedores. Atenciosamente, Felipe Mourão.";
+console.log(`%c${x}`, "font-size: 50px; color: red;");
+console.log(`%c${d}`, "font-size: 20px; color: #8B5CF6;");
